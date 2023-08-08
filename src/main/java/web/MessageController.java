76d3fbd08f6c1776f830d5971f0f0eabac0b8e23
unknown <leorizick@hotmail.com>
2023-08-07 23:29:49 -0300

    Create of CRUD

--- src/main/java/web/MessageController.java
+++ src/main/java/web/MessageController.java
@@ -1,6 +1,13 @@
 package web;
 
+import entities.DTO.MessageCreationRequest;
+import entities.DTO.MessageResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import services.api.services.message.MessageApiService;
 
@@ -10,4 +17,11 @@ public class MessageController {
 
     private MessageApiService messageApiService;
 
+    @PostMapping(value = "/api/message/{senderId}/{receiverId}")
+    public ResponseEntity<MessageResponse> sendMessage(@RequestBody MessageCreationRequest message, @PathVariable Long senderId, Long receiverId){
+        MessageResponse messageResponse = messageApiService.sendMessage(senderId, receiverId, message);
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body(messageResponse);
+    }
 }
